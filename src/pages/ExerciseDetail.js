82c815745e72fe7bc83994dbf1de0dc821bbe56d
Ayush Praveen <ayushpr.ap@outlook.com>
2023-07-27 20:34:24 +0530

    feat: added exercise details card

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+
+import { url, exerciseOptions, fetchData } from '../utils/fetchData'
+
+import Detail from '../components/Detail'
+import ExerciseVideos from '../components/ExerciseVideos'
+import SimilarExercises from '../components/SimilarExercises'
 
 const ExerciseDetail = () => {
-  return (
-    <div>ExerciseDetail</div>
-  )
+	const [exerciseDetail, setExerciseDetail] = useState({})
+	const {id} = useParams()
+
+	useEffect(() => {
+		const fetchExerciseData = async () => {
+			const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com'
+
+			const exerciseDetail = await fetchData(`${url}/exercise/${id}`, exerciseOptions)
+			setExerciseDetail(exerciseDetail)
+		}
+
+		fetchExerciseData()
+	}, [id])
+
+	return (
+		<Box>
+			<Detail exerciseDetail={exerciseDetail}/>
+			<ExerciseVideos />
+			<SimilarExercises />
+		</Box>
+	)
 }
 
 export default ExerciseDetail
